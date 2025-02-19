c3e6a08daa19d32a02365c7704f917ab91f6b3bc
jadis <jadisjale@gmail.com>
2025-02-19 00:48:45 -0300

    chore(app): Arquivos iniciais do projeto

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import { Box, Text } from "@chakra-ui/react";
+import Button from './components/Button';
+import theme from './theme/theme';
+import Input from "./components/Input";
+import ClientInfo from "./components/ClientInfo";
+
+import './App.css'
+
+function App() {
+  return (
+    // <Box 
+    //   p={4} 
+    //   w="100vw" 
+    //   h="100vh" 
+    //   display="flex" 
+    //   flexDirection="column"
+    //   alignItems="center" 
+    //   justifyContent="center"
+    //   gap={4}
+    // >
+    // <Text 
+    //   fontFamily="Inter"        
+    //   fontWeight={400}         
+    //   fontSize="36px"           
+    //   lineHeight="43.57px"     
+    //   letterSpacing="0%"
+    //   color={theme.colors.text} 
+    // >
+    //   {theme.labels.welcome}
+    // </Text>
+
+    //   <Input
+    //     placeholder={theme.inputs.name} 
+    //     size="lg" 
+    //     width="50%" 
+    //     bg={theme.colors.white}
+    //     color={theme.colors.text}
+    //     textAlign="center"
+    //   />
+
+    //   <Button 
+    //     label={theme.labels.enter}
+    //     size="2xl" 
+    //     onClick={() => alert("Botão clicado!")}
+    //     backgroundColor={theme.colors.primary}
+    //     w="50%"
+    //   />
+    // </Box>
+
+    <ClientInfo></ClientInfo>
+  );
+}
+
+export default App;
