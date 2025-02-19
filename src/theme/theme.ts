4dca99aac71351a4fc2e17af8c0fd363a088dbea
jadis <jadisjale@gmail.com>
2025-02-19 00:48:07 -0300

    chore(theme): Definicao inicial de texto e cores

---
+++ src/theme/theme.ts
@@ -0,0 +1,23 @@
+const theme = {
+  colors: {
+    primary: "rgba(236, 103, 36, 1)",
+    secondary: "#2ECC71",
+    background: "#F4F4F4",
+    text: "rgba(0, 0, 0, 1)",
+    buttonText: "rgba(255, 255, 255, 1)",
+    white: "white",
+    border: "rgba(217, 217, 217, 1)",
+    red: "red",
+  },
+  labels: {
+    welcome: "Olá, seja bem-vindo!",
+    enter: "Entrar",
+    wage: "Salário: ",
+    enterprise: "Empresa: ",
+  },
+  inputs: {
+    name: "Digite o seu nome:",
+  },
+};
+
+export default theme;
