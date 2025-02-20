d856e2e9806f5daf604918d84e123bf8597359ee
jadis <jadisjale@gmail.com>
2025-02-20 08:26:16 -0300

    feat(component): Componente de modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -7,6 +7,9 @@ import {
   ModalBody,
   ModalCloseButton,
 } from "@chakra-ui/modal";
+import { Flex, Text } from "@chakra-ui/react";
+
+import theme from "../../theme/theme";
 
 interface ModalProps {
   isOpen: boolean;
@@ -18,11 +21,49 @@ interface ModalProps {
 export const Modal = ({ isOpen, onClose, title, children }: ModalProps) => {
   return (
     <ChakraModal isOpen={isOpen} onClose={onClose} isCentered>
-      <ModalOverlay />
-      <ModalContent p={4}>
-        <ModalHeader>{title}</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>{children}</ModalBody>
+      <ModalOverlay bg="rgba(0, 0, 0, 0.8)" />
+      <ModalContent
+        maxW="350px"
+        minW="auto"
+        borderRadius="4px"
+        left={"37%"}
+        top={"35%"}
+        position={"fixed"}
+      >
+        <Flex
+          justify="space-between"
+          align="center"
+          w="100%"
+          p={"14px 14px 0px 14px"}
+          bg={theme.colors.white}
+        >
+          <ModalHeader flex={1}>
+            <Text
+              fontFamily={"Inter"}
+              fontWeight={"bold"}
+              fontSize={"16px"}
+              lineHeight="1.2"
+            >
+              {title}
+            </Text>
+          </ModalHeader>
+          <ModalCloseButton
+            size="sm"
+            position="static"
+            p={"0px"}
+            _hover={{
+              backgroundColor: theme.colors.transparent,
+              outline: "none",
+              boxShadow: "none",
+              borderColor: theme.colors.transparent,
+            }}
+            _focus={{ boxShadow: "none", outline: "none" }}
+          />
+        </Flex>
+
+        <ModalBody bg={theme.colors.white} p={"14px"}>
+          {children}
+        </ModalBody>
       </ModalContent>
     </ChakraModal>
   );
